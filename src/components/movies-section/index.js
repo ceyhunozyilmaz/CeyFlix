a84b1c466b57e9459c3ec3246510ec31b0be052e
ceyhunozyilmaz <sanatsalsaat18@gmail.com>
2024-03-28 17:47:41 +0300

    loading and categories added

--- src/components/movies-section/index.js
+++ src/components/movies-section/index.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -26,6 +25,5 @@ function MoviesSection({ title, movies }) {
     </div>
   );
 }
-
 
 export { MoviesSection };
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -7,11 +7,21 @@ import { FeaturedMovie } from "@/components/featured-movie";
 import { Categories } from "@/components/categories";
 import { MoviesSection } from "@/components/movies-section";
 
-function HomeContainer() {
+function HomeContainer({ selectedCategory }) {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[3]} />
       <Categories categories={Genres.genres.slice(0, 5)} />
+      {selectedCategory.movies.length > 0 && (
+        <MoviesSection
+          title={
+            Genres.genres.find((genre) => `${genre.id}` === selectedCategory.id)
+              .name
+          }
+          movies={selectedCategory.movies}
+        />
+      )}
+
       <MoviesSection title="En yeniler " movies={Movies.results.slice(1, 7)} />
       <MoviesSection
         title="En çok izlenen"
